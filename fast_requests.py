fbbb57fddaf5f4335f1ba35fdd46751d8a8df87a
MPS412 <m.b.paulus@vu.nl>
2022-02-02 15:17:21 +0100

    args as list of dicts with id

--- fast_requests.py
+++ fast_requests.py
@@ -20,21 +20,16 @@ def fast_requests(max_workers):
     """
     def decorator(f):
         # this is the actual decorator
-        def wrapped(urls: str, params: list, accept_codes: list,
-                    max_retry: int = 3, rate_limit: tuple = (5, 1), **kwargs):
+        def wrapped(reqs, accept_codes: list, max_retry: int = 3, rate_limit: tuple = (5, 1)):
             """ sends (chunks of) parallel get/post requests, adhering to API limits
-            :param urls
-            :param params: request parameters (list, len(params) == len(urls))
+            :param reqs: list of dicts {id, url, params, (headers), (data)}}
             :param accept_codes: list of API status codes that are acceptable
             :param max_retry: in case of an error
             :param rate_limit: tuple (number of requests, seconds)
-            :param kwargs: optional headers (dict), data (list, len(data) == len(params))
             """
-            results = []
+            results = {}
             n = rate_limit[0]  # chunk size
-            chunks = [range(len(urls))[i:i+n] for i in range(0, len(urls), n)]
-            headers = kwargs.get('headers', None)
-            data = kwargs.get('data', [None]*len(urls))
+            chunks = [reqs[i:i + n] for i in range(0, len(reqs), n)]
             with FuturesSession(max_workers=max_workers) as session:
                 chunk_idx = 0
                 trial = 0
@@ -42,17 +37,22 @@ def fast_requests(max_workers):
                     results_chunk = []
                     start = time.time()
                     # Future requests are run (in parallel) in the background
-                    futures = [f(session, urls[i],
-                                 headers=headers, params=params[i],
-                                 data=data[i]) for i in chunks[chunk_idx]]
+                    futures = []
+                    for req in chunks[chunk_idx]:
+                        future = f(session, req['url'],
+                                   headers=req.get('headers', None),
+                                   params=req.get('params', None),
+                                   data=req.get('data', None))
+                        future.i = req['id']
+                        futures.append(future)
                     # ensure that responses came back before continuing (to not overload the API)
                     for future in as_completed(futures):
-                        results_chunk.append(future.result())
+                        results_chunk.append(future)
                         # note: results won't be in list order but in order of completion
                     # wait the remaining time
                     time.sleep(max(0, rate_limit[1] - (time.time() - start)))
                     # check for unexpected status codes
-                    status_codes = [res.status_code for res in results_chunk]
+                    status_codes = [future.result().status_code for future in results_chunk]
                     if len(set(status_codes).difference(set(accept_codes))) != 0:
                         if trial < max_retry:
                             # retry chunk, otherwise stop
@@ -63,7 +63,8 @@ def fast_requests(max_workers):
                             # return results up to now and error messages
                             return results, f'unacceptable status codes: {status_codes}'
                     else:
-                        results = results + results_chunk
+                        for future in results_chunk:
+                            results[future.i] = future.result()
                         chunk_idx += 1  # next chunk
                         trial = 0  # reset trial when success
             return results, 'success'
--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 from fast_requests import fast_get
 import config
 
-
 with open('example_dois.txt', 'r') as f:
     doi_list = f.read().splitlines()
 
-urls = [f'https://api.unpaywall.org/v2/{doi}' for doi in doi_list]
-params = [{'email': config.my_email} for doi in doi_list]
+reqs = [{'id': doi,
+         'url': f'https://api.unpaywall.org/v2/{doi}',
+         'params': {'email': config.my_email}}
+        for doi in doi_list]
 
 rate_limit = (10, 1)
-
-(results, message), elapsed = fast_get(urls=urls, params=params, accept_codes=[200], max_retry=3, rate_limit=rate_limit)
+(results, message), elapsed = fast_get(reqs, accept_codes=[200], max_retry=3, rate_limit=rate_limit)
